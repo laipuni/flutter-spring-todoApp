a1fec6734aa1f90278100e97abfadb8d58b4986f
laipuni <2tgb02023@gmail.com>
2025-02-12 18:52:31 +0900

    refactor : TodoControllerTest에 @WebMvcTest로 변경

--- src/test/java/project/app/flutter_spring_todoapp/todo/controller/TodoControllerTest.java
+++ src/test/java/project/app/flutter_spring_todoapp/todo/controller/TodoControllerTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
@@ -35,8 +33,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = {
+        TodoController.class
+})
 class TodoControllerTest {
 
     @Autowired
