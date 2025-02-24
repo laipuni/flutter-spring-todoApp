5aa5ea1c530c0facf535697fe14b8bad64c73c44
laipuni <2tgb02023@gmail.com>
2025-02-24 19:52:55 +0900

    feat : Todo에 member n : 1 연관관계 추가

--- src/main/java/project/app/flutter_spring_todoapp/todo/domain/Todo.java
+++ src/main/java/project/app/flutter_spring_todoapp/todo/domain/Todo.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.util.StringUtils;
+import project.app.flutter_spring_todoapp.member.Member;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -36,28 +37,33 @@ public class Todo {
     @Enumerated(EnumType.STRING)
     private TodoPriority priority;
 
+    @JoinColumn(name = "member_id",nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
     @Builder
     private Todo(final String title, final String description,
                  final LocalDateTime startDate, final LocalDateTime dueDate,
-                 final TodoStatus status, final TodoPriority priority) {
+                 final TodoStatus status, final TodoPriority priority, final Member member) {
         this.title = title;
         this.description = description;
         this.startDate = startDate;
         this.dueDate = dueDate;
         this.status = status;
         this.priority = priority;
+        this.member = member;
     }
 
     public static Todo of(final String title, final String description,
                           final LocalDateTime startDate, final LocalDateTime dueDate,
-                          final TodoPriority priority){
+                          final TodoPriority priority, final Member member){
 
-        return of(title,description,startDate,dueDate,null,priority);
+        return of(title,description,startDate,dueDate,null,priority,member);
     }
 
     public static Todo of(final String title, final String description,
                           final LocalDateTime startDate, final LocalDateTime dueDate,
-                          TodoStatus status, final TodoPriority priority
+                          TodoStatus status, final TodoPriority priority,final Member member
     ){
         if(status == null){
             //사용자가 상태를 설정하지 않았을 경우 지정한 날짜에 따라 상태를 지정함
@@ -71,6 +77,7 @@ public class Todo {
                 .dueDate(dueDate)
                 .status(status)
                 .priority(priority)
+                .member(member)
                 .build();
     }
 
@@ -107,6 +114,10 @@ public class Todo {
     public void changeDate(final LocalDateTime startDate, final LocalDateTime dueDate){
         changeStartDate(startDate);
         changeDueDate(dueDate);
+    }
+
+    public boolean isWriter(final Long memberId){
+        return this.member.getId().equals(memberId);
     }
 
     public void done(){
