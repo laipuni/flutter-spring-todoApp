1a2c4c2dba958541c2b2ab62c0f1cebd41ff5fad
laipuni <2tgb02023@gmail.com>
2025-02-24 19:58:17 +0900

    fix : SessionMemberArgumentResolver 빈 등록

--- src/main/java/project/app/flutter_spring_todoapp/web/config/WebMvcConfig.java
+++ src/main/java/project/app/flutter_spring_todoapp/web/config/WebMvcConfig.java
@@ -1,17 +1,42 @@
-package project.app.flutter_spring_todoapp.web.converter.config;
+package project.app.flutter_spring_todoapp.web.config;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import project.app.flutter_spring_todoapp.todo.domain.TodoPriority;
 import project.app.flutter_spring_todoapp.todo.domain.TodoStatus;
 import project.app.flutter_spring_todoapp.web.converter.*;
+import project.app.flutter_spring_todoapp.web.resolver.SessionMemberArgumentResolver;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    HttpSession httpSession;
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(sessionMemberArgumentResolver());
+    }
+
+    @Override
+    public void addFormatters(final FormatterRegistry registry) {
+        registry.addConverter(todoStatusToStringConverter());
+        registry.addConverter(stringToTodoStatusConverter());
+        registry.addConverter(todoPriorityToStringConverter());
+        registry.addConverter(stringToTodoPriorityConverter());
+    }
+
+    public HandlerMethodArgumentResolver sessionMemberArgumentResolver(){
+        return new SessionMemberArgumentResolver(httpSession);
+    }
 
     public Converter<TodoStatus, String> todoStatusToStringConverter(){
         return new TodoStatusToStringConverter();
@@ -27,14 +52,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     public Converter<String, TodoPriority> stringToTodoPriorityConverter(){
         return new StringToTodoPriorityConverter();
-    }
-
-
-    @Override
-    public void addFormatters(final FormatterRegistry registry) {
-        registry.addConverter(todoStatusToStringConverter());
-        registry.addConverter(stringToTodoStatusConverter());
-        registry.addConverter(todoPriorityToStringConverter());
-        registry.addConverter(stringToTodoPriorityConverter());
     }
 }
