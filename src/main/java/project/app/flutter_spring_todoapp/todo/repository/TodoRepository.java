2b840c74c0d3f229af7029c7c4e906fb2270180d
laipuni <2tgb02023@gmail.com>
2025-03-02 13:08:29 +0900

    feat : 할일 상세 정보를 조회 시 Dto로 받도록 변경

--- src/main/java/project/app/flutter_spring_todoapp/todo/repository/TodoRepository.java
+++ src/main/java/project/app/flutter_spring_todoapp/todo/repository/TodoRepository.java
@@ -2,12 +2,24 @@ package project.app.flutter_spring_todoapp.todo.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.app.flutter_spring_todoapp.todo.domain.Todo;
+import project.app.flutter_spring_todoapp.todo.dto.response.DetailTodoResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     List<Todo> findAllByMemberIdOrderByIdDesc(Long memberId);
+
+    @Query("select new project.app.flutter_spring_todoapp.todo.dto.response." +
+            "DetailTodoResponse(t.id, t.title, t.description, t.startDate, t.dueDate, t.status, t.priority, n.timeType) " +
+            "from Todo t " +
+            "left join Notification n on n.todo.id = t.id " +
+            "where t.id = :todoId"
+    )
+    Optional<DetailTodoResponse> findTodoDetailByTodoId(@Param("todoId") Long todoId);
 
 }
