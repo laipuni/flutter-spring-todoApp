ef8c89f83f29d7f6c4d677a49c4cd72fdc5eae59
laipuni <2tgb02023@gmail.com>
2025-03-02 13:51:51 +0900

    feat : main에 BoardScreen을 분리 및 라우터 설정

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,15 +1,13 @@
 import 'dart:convert';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:todo_app/auth/AuthService.dart';
+import 'package:todo_app/RouteName.dart';
 import 'package:todo_app/fcm/PushNotification.dart';
+import 'package:todo_app/service/SharedPreferencesService.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
-String? _token;
+final SharedPreferencesService _sharedPreferencesService = SharedPreferencesService();
 
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -20,216 +18,45 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 Future<void> setupInteractedMessage() async {
   RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
-
   // 앱을 껐다 켰을 경우
   if (initialMessage != null) {
     _handleMessage(initialMessage);
   }
-
   // 앱이 백그라운드 상태에서 사용자가 알림을 탭할 때 핸들러 지정
   FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
 }
 
 void _handleMessage(RemoteMessage message) {
   Future.delayed(const Duration(seconds: 1), () {
-    navigatorKey.currentState!.pushNamed("/message", arguments: message);
+    print("Notification Received, msg = ${message.notification}");
   });
+}
+
+void handlePushNotification(RemoteMessage message) {
+  if (message.notification != null) {
+    PushNotification.showNotification(message);
+  }
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  _token = await PushNotification.init();
-  print("파이어 베이스 토큰: $_token");
+  String token = await PushNotification.init();
+  _sharedPreferencesService.saveFcmToken(token);
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   setupInteractedMessage();
-
   // Foreground 푸시 알림 처리
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    if (message.notification != null) {
-      PushNotification.showNotification(message);
-    }
-  });
-
-  sendToken(_token);
-
+  FirebaseMessaging.onMessage.listen(handlePushNotification);
   runApp(_MyApp());
-}
-
-Future<void> sendToken(token) async {
-  const String apiUrl = "http://10.0.2.2:8080/send"; // Android 에뮬레이터
-  try {
-    http.post(
-      Uri.parse(apiUrl),
-      headers: {"Content-Type": "application/json"},
-      body: jsonEncode({'token': token}),
-    );
-  } catch (e) {
-    print("네트워크 오류 발생: $e");
-  }
 }
 
 class _MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: navigatorKey,
-      debugShowCheckedModeBanner: false,
-      home: _BoardScreen(),
+      routes: routeName,
+      initialRoute: RouteName.home,
     );
   }
 }
 
-class _BoardScreen extends StatefulWidget {
-  @override
-  _BoardScreenState createState() => _BoardScreenState();
-}
-
-class _BoardScreenState extends State<_BoardScreen> {
-  final List<Map<String, String>> _posts = []; // 게시글 목록
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _contentController = TextEditingController();
-  String serverMessage = "서버 응답 없음"; // 서버에서 받아온 데이터
-  final AuthService _authService = AuthService();
-  User? _currentUser;
-
-  /// 🔹 Google 로그인 실행
-  Future<void> _handleGoogleLogin() async {
-    User? user = await _authService.signInWithGoogle();
-    if (user != null) {
-      String? idToken = await user.getIdToken();
-      await _authService.sendTokenToBackend(idToken, _token);
-      setState(() {
-        _currentUser = user;
-      });
-    }
-  }
-
-  /// 🔹 로그아웃 실행
-  Future<void> _handleLogout() async {
-    await _authService.signOut();
-    setState(() {
-      _currentUser = null;
-    });
-  }
-
-  void _addPost() {
-    String title = _titleController.text.trim();
-    String content = _contentController.text.trim();
-
-    if (title.isNotEmpty && content.isNotEmpty) {
-      setState(() {
-        _posts.add({"title": title, "content": content});
-      });
-      _titleController.clear();
-      _contentController.clear();
-      Navigator.of(context).pop();
-    }
-  }
-
-  void _showAddPostDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("새 게시글 추가"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _titleController,
-                decoration: InputDecoration(labelText: "제목"),
-              ),
-              TextField(
-                controller: _contentController,
-                decoration: InputDecoration(labelText: "내용"),
-                maxLines: 3,
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text("취소"),
-            ),
-            TextButton(
-              onPressed: _addPost,
-              child: Text("추가"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("게시판")),
-      body: Column(
-        children: [
-          // 🌐 Google 로그인 버튼 추가
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: _currentUser == null
-                ? ElevatedButton.icon(
-              icon: Icon(Icons.login),
-              label: Text("Google 로그인"),
-              onPressed: _handleGoogleLogin,
-            )
-                : Column(
-              children: [
-                Text("환영합니다, ${_currentUser!.displayName}님"),
-                SizedBox(height: 10),
-                ElevatedButton.icon(
-                  icon: Icon(Icons.logout),
-                  label: Text("로그아웃"),
-                  onPressed: _handleLogout,
-                ),
-              ],
-            ),
-          ),
-
-          // 🌐 서버에서 받아온 메시지 표시
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("서버 응답: $serverMessage",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-          ),
-
-          // 📜 게시글 목록
-          Expanded(
-            child: _posts.isEmpty
-                ? Center(child: Text("게시글이 없습니다."))
-                : ListView.builder(
-              itemCount: _posts.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: EdgeInsets.all(8),
-                  child: ListTile(
-                    title: Text(_posts[index]["title"]!),
-                    subtitle: Text(_posts[index]["content"]!),
-                  ),
-                );
-              },
-            ),
-          ),
-
-          // 🌐 API 호출 버튼 추가
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () => sendToken(_token),
-              child: Text("서버에서 데이터 가져오기"),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showAddPostDialog,
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-}
---
+++ todo_app/lib/screen/BoardScreen.dart
@@ -0,0 +1,124 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:todo_app/HostName.dart';
+import 'package:todo_app/RouteName.dart';
+import 'package:todo_app/auth/AuthService.dart';
+import 'package:todo_app/enum/TodoStatus.dart';
+import 'package:todo_app/model/Todo.dart';
+import 'package:todo_app/service/SecureStorageService.dart';
+import 'package:todo_app/service/SharedPreferencesService.dart';
+import 'package:http/http.dart' as http;
+import 'package:todo_app/util/DateTimeUtils.dart';
+
+class BoardScreen extends StatefulWidget {
+  @override
+  _BoardScreenState createState() => _BoardScreenState();
+}
+
+class _BoardScreenState extends State<BoardScreen> {
+  List<TodoView> _todoList = [];
+  bool _isLoading = true;
+  final SecureStorageService _secureStorageService = SecureStorageService();
+
+  @override
+  void initState() {
+    super.initState();
+    receiveTodoList();
+  }
+
+  Future<void> receiveTodoList() async {
+    var url = Uri.parse("${HostName.host}/api/todos");
+    String? idToken = await _secureStorageService.getAccessToken();
+
+    http.Response response = await http.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $idToken',
+      },
+    );
+
+    setTodoList(response);
+  }
+
+  void setTodoList(http.Response response) {
+    Map<String, dynamic> responseMap = json.decode(response.body);
+    List<dynamic> todosJson = responseMap["data"]["todoList"];
+
+    setState(() {
+      _todoList = todosJson.map((todo) => TodoView.fromJson(todo)).toList();
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("할 일 목록", style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              AuthService().signOut();
+              Navigator.pushReplacementNamed(context, RouteName.login);
+            },
+          ),
+        ],
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : _todoList.isEmpty
+          ? Center(child: Text("등록된 할 일이 없습니다.", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)))
+          : Padding(
+        padding: EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: _todoList.length,
+          itemBuilder: (context, index) {
+            return _buildTodoCard(_todoList[index]);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => Navigator.pushNamed(context, RouteName.todoAdd),
+        icon: Icon(Icons.add),
+        label: Text("추가하기"),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+      ),
+    );
+  }
+
+  Widget buildTodoStatusIcon(TodoStatus status){
+    if(status == TodoStatus.done){
+      return Icon(Icons.check_circle_outline, color: Colors.green, size: 32);
+    } else if (status == TodoStatus.inProgress){
+      return Icon(Icons.circle_notifications_outlined, color: Colors.blueAccent, size: 32);
+    } else {
+      return Icon(Icons.circle_outlined, color: Colors.grey, size: 32);
+    }
+  }
+
+  Widget _buildTodoCard(TodoView todo) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      elevation: 4,
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(12),
+        leading: buildTodoStatusIcon(todo.status),
+        title: Text(todo.title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        subtitle: Text(
+          "${DateTimeUtils.showDateTime(todo.startTime)} ~ ${DateTimeUtils.showDateTime(todo.dueTime)}",
+          style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+        ),
+        trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey[600]),
+        onTap: () {
+          Navigator.pushNamed(context, RouteName.todoDetail, arguments: todo.id);
+        },
+      ),
+    );
+  }
+}
