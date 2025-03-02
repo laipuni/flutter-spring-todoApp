1a28491501371a6e0c94548056df05bf8ee588cf
laipuni <2tgb02023@gmail.com>
2025-03-02 13:16:51 +0900

    feat : 할일 상세 조회 응답값에 timeType을 추가

--- src/main/java/project/app/flutter_spring_todoapp/todo/dto/response/DetailTodoResponse.java
+++ src/main/java/project/app/flutter_spring_todoapp/todo/dto/response/DetailTodoResponse.java
@@ -1,8 +1,10 @@
 package project.app.flutter_spring_todoapp.todo.dto.response;
 
+import jakarta.annotation.Nullable;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import project.app.flutter_spring_todoapp.notification.TimeType;
 import project.app.flutter_spring_todoapp.todo.domain.Todo;
 import project.app.flutter_spring_todoapp.todo.domain.TodoPriority;
 import project.app.flutter_spring_todoapp.todo.domain.TodoStatus;
@@ -21,11 +23,12 @@ public class DetailTodoResponse {
     private LocalDateTime dueDate;
     private TodoStatus status;
     private TodoPriority priority;
+    private TimeType timeType;
 
     @Builder
     private DetailTodoResponse(final Long id, final String title, final String description,
                             final LocalDateTime startDate, final LocalDateTime dueDate,
-                            final TodoStatus status, final TodoPriority priority) {
+                            final TodoStatus status, final TodoPriority priority, @Nullable final TimeType timeType) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -33,9 +36,10 @@ public class DetailTodoResponse {
         this.dueDate = dueDate;
         this.status = status;
         this.priority = priority;
+        this.timeType = timeType != null ? timeType : TimeType.NONE;
     }
 
-    public static DetailTodoResponse of(final Todo todo) {
+    public static DetailTodoResponse of(final Todo todo,final TimeType timeType) {
         return DetailTodoResponse.builder()
                 .id(todo.getId())
                 .title(todo.getTitle())
@@ -44,6 +48,7 @@ public class DetailTodoResponse {
                 .dueDate(todo.getDueDate())
                 .status(todo.getStatus())
                 .priority(todo.getPriority())
+                .timeType(timeType)
                 .build();
     }
 }
