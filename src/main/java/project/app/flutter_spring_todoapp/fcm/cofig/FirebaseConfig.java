b7d3f936108aeb8aa64b353db52804cebb7a1651
laipuni <2tgb02023@gmail.com>
2025-03-10 13:57:09 +0900

    fix : firebaseApp 중복으로 초기화로 예외 해결

--- src/main/java/project/app/flutter_spring_todoapp/fcm/cofig/FirebaseConfig.java
+++ src/main/java/project/app/flutter_spring_todoapp/fcm/cofig/FirebaseConfig.java
@@ -9,19 +9,37 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
+
+        FirebaseApp firebaseApp = null;
+        boolean hasBeenInitialized = false;
+        for(FirebaseApp app : FirebaseApp.getApps()){
+            if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                hasBeenInitialized = true;
+                firebaseApp = app;
+            }
+        }
+        if(!hasBeenInitialized){
+            firebaseApp = getFirebaseApp();
+        }
+        return firebaseApp;
+    }
+
+    private static FirebaseApp getFirebaseApp() throws IOException {
+        FirebaseApp firebaseApp;
         FileInputStream serviceAccount =
                 new FileInputStream("src/main/resources/todoapp-3faa2-firebase-adminsdk-fbsvc-cc78a801d0.json");
-
-        FirebaseOptions options = FirebaseOptions.builder()
+        FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
-        return FirebaseApp.initializeApp(options);
+        firebaseApp = FirebaseApp.initializeApp(options);
+        return firebaseApp;
     }
 
     @Bean
