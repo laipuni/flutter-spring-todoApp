0e7becf4e4b01b1d5a8617e527086497825b67ae
laipuni <2tgb02023@gmail.com>
2025-03-11 21:51:04 +0900

    refactor : FirebaseAuthenticationFilter 리팩토링

--- src/main/java/project/app/flutter_spring_todoapp/security/filter/FirebaseAuthenticationFilter.java
+++ src/main/java/project/app/flutter_spring_todoapp/security/filter/FirebaseAuthenticationFilter.java
@@ -8,14 +8,23 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 import project.app.flutter_spring_todoapp.exception.fcm.FailedFirebaseInitException;
 import project.app.flutter_spring_todoapp.exception.global.LoginRequiredException;
@@ -23,13 +32,15 @@ import project.app.flutter_spring_todoapp.member.Member;
 import project.app.flutter_spring_todoapp.member.repository.MemberRepository;
 import project.app.flutter_spring_todoapp.security.oauth2.dto.SessionMember;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 public class FirebaseAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String JWT_TOKEN_HEADER = "Bearer ";
 
     private final MemberRepository memberRepository;
     private final FirebaseAuth firebaseAuth;
@@ -40,14 +51,13 @@ public class FirebaseAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        String header = request.getHeader("Authorization");
-        if (header == null || !header.startsWith("Bearer ")) {
-            chain.doFilter(request, response);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader(AUTHORIZATION_HEADER);
+        if (header == null || !header.startsWith(JWT_TOKEN_HEADER)) {
+            chain.doFilter(request,response);
             return;
         }
-        String idToken = header.replace("Bearer ", "");
+        String idToken = header.replace(JWT_TOKEN_HEADER, "");
         FirebaseToken decodedToken = getFirebaseToken(response, idToken);
         memberRepository.findMemberByEmail(decodedToken.getEmail())
                 .ifPresent(FirebaseAuthenticationFilter::setAuthentication);
@@ -67,9 +77,8 @@ public class FirebaseAuthenticationFilter extends OncePerRequestFilter {
         List<SimpleGrantedAuthority> authorities = Collections.singletonList(
                 new SimpleGrantedAuthority(member.getRoleName()) // 사용자의 권한을 Holder에 저장하기 위해 생성
         );
-        //SecurityContext에 사용자 정보 저장
-        UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(SessionMember.of(member), null, authorities);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        //SecurityContextHolder에 사용자 정보 저장
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(SessionMember.of(member), null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(token);
     }
 }
