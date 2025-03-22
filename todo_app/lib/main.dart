fbf022f3e8a8fbb5eddf23adfdea345f9910c97d
laipuni <2tgb02023@gmail.com>
2025-03-22 13:39:28 +0900

    refactor : 디버깅 표시 제거

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -59,6 +59,7 @@ class _MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: routeName,
       initialRoute: RouteName.home,
     );
